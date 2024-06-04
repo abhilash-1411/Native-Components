5774fd246925fb39872dba0d76527a516c4ec488
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-06-04 17:11:19 +0530

    Components

---
+++ components/CompanyData.js
@@ -0,0 +1,11 @@
+import { View,Text } from "react-native"
+
+export default  CompanyData=()=>{
+    return(
+      <View>
+        <Text style={{fontSize:20}}>Name:Adixoo</Text>
+        <Text style={{fontSize:20}}>Employee:25</Text>
+        <Text style={{fontSize:20}}>Email:adixoo@gmail.com</Text>
+      </View>
+    )
+  }
